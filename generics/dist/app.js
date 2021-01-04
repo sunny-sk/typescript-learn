ec3de8a101b96aaadf7dac3a7c819c6bede19dcb
sunny-sk <sk956223@gmail.com>
2021-01-04 21:17:23 +0530

    generic finished

--- generics/dist/app.js
+++ generics/dist/app.js
@@ -3,5 +3,52 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 const mergeObj = merge({ name: 'sunny' }, { age: 12 });
-console.log(mergeObj.age);
+function countAndDescribe(element) {
+    let x = 'Got no value';
+    if (element.length > 0) {
+        x = 'Got' + element.length + 'elements';
+    }
+    return [element, x];
+}
+function extractAndConvert(obj, key) {
+    return 'value of ' + obj[key];
+}
+console.log(extractAndConvert({ name: 'sunny' }, 'name'));
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('max');
+textStorage.addItem('sunny');
+textStorage.removeItem('max');
+console.log(textStorage.getItems());
+const textStorage1 = new DataStorage();
+textStorage1.addItem(1);
+textStorage1.addItem(3);
+textStorage1.removeItem(1);
+console.log(textStorage1.getItems());
+const objectStorage = new DataStorage();
+objectStorage.addItem({ name: 'max' });
+objectStorage.addItem({ name: 'menu' });
+objectStorage.removeItem({ name: 'max' });
+console.log(objectStorage.getItems());
+function createGoal(title, description, date) {
+    let CourseGoals = {};
+    CourseGoals.title = title;
+    CourseGoals.description = description;
+    CourseGoals.completeUntil = date;
+    return CourseGoals;
+}
+const names = ['max', 'menu'];
 //# sourceMappingURL=app.js.map
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -20,7 +20,7 @@
 
 // Generic function
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends Object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
@@ -34,5 +34,84 @@ const mergeObj = merge<{ name: string }, { age: number }>(
   { name: 'sunny' },
   { age: 12 }
 );
+// const mergeObj1 = merge({ name: 'sunny' }, 3);
 
-console.log(mergeObj.age);
+// console.log(mergeObj.age);
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let x = 'Got no value';
+  if (element.length > 0) {
+    x = 'Got' + element.length + 'elements';
+  }
+  return [element, x];
+}
+
+// console.log(countAndDescribe('Hi there'));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'value of ' + obj[key];
+}
+
+console.log(extractAndConvert({ name: 'sunny' }, 'name'));
+
+// Generic classes
+
+class DataStorage<T> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('max');
+textStorage.addItem('sunny');
+textStorage.removeItem('max');
+console.log(textStorage.getItems());
+
+const textStorage1 = new DataStorage<number>();
+textStorage1.addItem(1);
+textStorage1.addItem(3);
+textStorage1.removeItem(1);
+console.log(textStorage1.getItems());
+
+const objectStorage = new DataStorage<object>();
+objectStorage.addItem({ name: 'max' });
+objectStorage.addItem({ name: 'menu' });
+objectStorage.removeItem({ name: 'max' });
+console.log(objectStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let CourseGoals: Partial<CourseGoal> = {};
+
+  CourseGoals.title = title;
+  CourseGoals.description = description;
+  CourseGoals.completeUntil = date;
+  return CourseGoals as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['max', 'menu'];
+// names.push('anna');
+// names.push();
