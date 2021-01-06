ec8292dd165b1c30db149e68dd9d5754953c244d
sunny-sk <sk956223@gmail.com>
2021-01-06 20:12:00 +0530

    types of decorators

--- decrators/dist/app.js
+++ decrators/dist/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Loggers(_) {
     console.log('Logging...');
 }
@@ -37,4 +40,53 @@ Person = __decorate([
 ], Person);
 const pers = new Person();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log('PropertyDecorator ');
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log('Accessor Decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('Method Decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('Perameter Decorator');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('invalid price');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
--- decrators/src/app.ts
+++ decrators/src/app.ts
@@ -33,3 +33,62 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+//-------
+// propert decorator
+function Log(target: any, propertyName: string | symbol) {
+  console.log('PropertyDecorator ');
+  console.log(target, propertyName);
+}
+
+// accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor Decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+// method decorator
+function Log3(
+  target: any,
+  name: string | symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method Decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+// perameter decorator
+function Log4(target: any, name: string | symbol, position: number) {
+  console.log('Perameter Decorator');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('invalid price');
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
