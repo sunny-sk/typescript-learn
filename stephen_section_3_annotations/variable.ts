871909692752d9b9b8f95ef5946282b16f60cfeb
sunny-sk <sk956223@gmail.com>
2021-01-11 22:49:18 +0530

    variable anotations added

---
+++ stephen_section_3_annotations/variable.ts
@@ -0,0 +1,29 @@
+let apples: number = 5; // 3.5 // -21
+let speed: string = 'fast'; // "sunnt"
+let check: boolean = true; // false
+let nothingMuch: null = null; // undefined // null
+let nothing: undefined = undefined; // undefined // null
+
+// built in object =
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'blue'];
+let myNumbers: number[] = [2, -12, 2.4];
+let truths: boolean[] = [true, false];
+
+//classes
+class Car {}
+
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// function
+const logNUmber: (i: number) => void = (i) => {
+  console.log(i);
+};
