2cc97de59afdafdf1a6570b22de6a65d14bc7017
sunny-sk <sk956223@gmail.com>
2021-01-12 22:04:17 +0530

    arrays types added

---
+++ Stephen_mastering_typed_array/arrays.ts
@@ -0,0 +1,18 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date(), new Date()];
+
+//2D array
+const matrix = [
+  [1, 2, 3, 4],
+  [12, 4, 5],
+  [2, 4, 5],
+];
+
+// help with map
+
+carMakers.map((e: string): string => {
+  return e;
+});
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
