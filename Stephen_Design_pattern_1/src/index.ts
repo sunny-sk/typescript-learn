5d7b4879cb9f9dc14b3dd0877fcd956e5b984526
sunny-sk <sk956223@gmail.com>
2021-01-15 23:45:43 +0530

    sorting added

--- Stephen_Design_pattern_1/src/index.ts
+++ Stephen_Design_pattern_1/src/index.ts
@@ -1,7 +1,20 @@
-console.log('Hello world');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log('something good');
-};
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; ++i) {
+      for (let j = 0; j < length - 1 - i; ++j) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          let temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
 
-logSomething();
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
